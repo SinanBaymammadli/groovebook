dfb767d77f3bc9b22b270d25327178a68b6202c8
Sinan Baymammadli <sinan.baymammadli@gmail.com>
2018-09-19 16:01:53 +0400

    init

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export default class App extends Component {
+  componentDidMount = () => {};
+
+  render() {
+    return (
+      <View>
+        <Text>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
